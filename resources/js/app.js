788c4c53766eb414c1e7162e294b4e321395fbde
khushboo-muliyana <khushboomuliyana28@gmail.com>
2025-08-23 15:13:11 +0530

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
